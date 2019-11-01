91d60f91dc0663004c372bb30407ec65e28752d6
mumrikcheb <57175947+mumrikcheb@users.noreply.github.com>
2019-11-01 22:41:09 +0300

    Add files via upload

---
+++ _Sensors_voltage.ino
@@ -0,0 +1,28 @@
+
+int voltage;
+float outvoltage;
+
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(2,INPUT);
+}
+
+
+void loop() { 
+  voltage = analogRead(0)+11;
+  outvoltage = map(voltage, 0, 1023, -512, 512);
+  outvoltage = outvoltage * 0.0098;
+  voltage = outvoltage * 1000;
+//  Serial.print(voltage);
+//  Serial.print("  ");
+//+
+
+
+Serial.print('$');
+Serial.print('v');
+Serial.print(voltage);
+Serial.println(';');
+delay(500);
+
+}
